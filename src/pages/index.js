d6d80c861174eab8b40afb33dc8d937a1f8ae5d7
gabrielts212 <gabrielts2122@gmail.com>
2024-06-02 15:47:38 -0300

    feat: header

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Header from "../components/header/header";
+
+// import Footer from "../components/footer/Footer";
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      {/* <Footer /> */}
+    
+    </div>
+  );
+}
