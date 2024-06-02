57bc4bf5573d4902ad579e35d6a2a4021b00e280
gabrielts212 <gabrielts2122@gmail.com>
2024-06-02 18:55:44 -0300

    feat: microsoft form 👍

--- src/pages/api/lib/msalConfig.js
+++ src/pages/api/lib/msalConfig.js
@@ -1,10 +1,20 @@
-import { PublicClientApplication } from "@azure/msal-browser";
+import { PublicClientApplication } from '@azure/msal-browser';
+
+const getRedirectUri = () => {
+  if (typeof window !== 'undefined') {
+    const isLocalhost = window.location.hostname === 'localhost';
+    return isLocalhost
+      ? 'http://localhost:3000/api/auth/microsoft/callback'
+      : 'https://gabrielts212.vercel.app/api/auth/microsoft/callback';
+  }
+  return 'http://localhost:3000/api/auth/microsoft/callback'; // Default for SSR
+};
 
 const msalConfig = {
   auth: {
-    clientId: "14f63b39-4241-4173-84e2-9c632e81ab64",
+    clientId: '14f63b39-4241-4173-84e2-9c632e81ab64',
     authority: 'https://login.microsoftonline.com/common',
-    redirectUri: "http://localhost:3000/api/auth/microsoft/callback",
+    redirectUri: getRedirectUri(),
   },
 };
 
