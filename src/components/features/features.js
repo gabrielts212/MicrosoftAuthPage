501aa710deb58db483b3d617329bc68fe34662d4
gabrielts212 <gabrielts2122@gmail.com>
2024-06-03 03:16:46 -0300

    feat: features e pricing

---
+++ src/components/features/features.js
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import React from "react";
+import Slider from "../../assets/slider.png";
+import Featuresimage from "../../assets/featuresimage.png";
+import Shape from "../../assets/shape.png";
+
+const Features = () => {
+  return (
+    <div className="relative bg-[#0c0c0c] text-white py-12 px-6 flex justify-center">
+      <div className="absolute inset-0">
+        <Image src={Shape} alt="Background" width={120} height={120} />
+      </div>
+      <div className="relative max-w-7xl w-full flex flex-col lg:flex-row items-center justify-between ">
+        <div className="w-full lg:w-1/2 text-center lg:text-left mb-12 lg:mb-0 lg:pr-8">
+          <h2 className="text-4xl font-bold mb-4">
+            Study Anywhere Easily Online
+          </h2>
+          <p className="text-gray-400 mb-6">
+            Discover the freedom of online learning. Our platform empowers you
+            to access quality education from the comfort of your home or on the
+            go.
+          </p>
+          <div className="flex justify-center lg:justify-start">
+            <Image
+              src={Slider}
+              alt="Left Image"
+              width={400}
+              height={400}
+              className="object-cover"
+            />
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2 flex justify-center lg:justify-end">
+          <Image
+            src={Featuresimage}
+            alt="Right Image"
+            width={550}
+            height={550}
+            className="object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Features;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,18 @@ import Classe from "../components/classe/classe";
 import Aboutus from "../components/aboutus/aboutus";
 import Header from "../components/header/header";
 import Footer from "../components/footer/footer";
+import Pricing from "../components/pricing/pricing";
+import Features from "../components/features/features";
 
-// import Favorite from "../components/favorite/favorite";
-// import Study from "../components/study/study";
 // import Learning from "../components/learning/learning";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[#0c0c0c]">
       <Header />
-      {/* <Learning/>
-      <Study />
-      <Favorite/> */}
+      {/* <Learning/> */}
+      <Features/>
+      <Pricing/>
       <Classe/>
       <Aboutus />
       <Footer />
